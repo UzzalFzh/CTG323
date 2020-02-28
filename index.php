2d27749330144309e08e0e28b1102f69843a0eb5
shan <shan@gmail.com>
2020-02-28 20:44:25 +0600

    second commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>class 5</title>
+	</head>
+	
+	<body>
+	<?php
+
+	if(isset($_POST['submit'])){
+	$tmp = $_FILES['image']['tmp_name'];
+	$img_name = uniqid().".jpg";
+	move_uploaded_file($tmp, "photos/".$img_name);
+}
+
+
+	?>
+
+
+	<form method= "POST" action="" enctype="multipart/form-data">
+		<input type="file" name="image">
+		<input type="submit" name="submit" value="upload">
+	</form>
+	
+	</body>
+</html>
+
